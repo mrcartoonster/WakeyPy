483cb00a81d634d63a296e91786158b2acad51e2
Evan Baird <evanbaird@evans-mbp.lan>
2024-06-09 17:27:11 -0600

    wake screen working

--- src/wakeypy/spinning.py
+++ src/wakeypy/spinning.py
@@ -1,10 +1,18 @@
 # -*- coding: utf-8 -*-
 import time
 
+import tomllib
 from rich.console import Console
 from wakepy import keep
 
-console = Console()
+console = Console(width=80)
+
+with open(
+    "/Users/evanbaird/Projects/Projects/wakeypy/pyproject.toml",
+    mode="rb",
+) as fp:
+    loading = tomllib.load(fp)
+    version = "v." + loading["tool"]["poetry"]["version"]
 
 
 def do_something():
@@ -24,5 +32,23 @@ def do_something():
 status = "[green]Press Ctrl-C to exit[/green]"
 
 
-with console.status(status, spinner="aesthetic"):
-    do_something()
+with console.status(status, spinner="dots"):
+    console.print(
+        rf"""
+                           _
+                          | |
+        __        __ __ _ | | __ ___  _   _  _ __    _   _
+        \ \  /\  / // _` || |/ // _ \| | | || '_  \ | | | |
+         \ \/  \/ /| (_| ||   <|  __/| |_| || |_)  || |_| |
+          \__/\__/  \__,_||_|\_\\___| \__, || .___/  \__, |
+          {version}                      __/ || |       __| |
+                                      |___/ |_|      |___/
+         """,
+        style="magenta",
+    )
+    console.print(
+        "[ ] System will continue running program\n"
+        "[ ] Presentation mode is on. Screensaver and Screenlock will be prevented.",
+        style="green",
+    )
+    {do_something()}
